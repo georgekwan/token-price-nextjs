7e41883570bd20d7becb76e7e9331f457252cf26
George <gwmkwan@gmail.com>
2023-03-03 18:07:25 -0700

    feat: create main

--- components/main.js
+++ components/main.js
@@ -1,1 +1,23 @@
 import { useState } from 'react';
+const Moralis = require('moralis').default;
+const { EvmChain } = require('@moralisweb3/common-evm-utils');
+import Select from 'react-select';
+import styles from '../styles/Home.module.css';
+
+export default function Header() {
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState('');
+  const [chainValue, setChainValue] = useState('');
+
+  return (
+    <section>
+      <form>
+        <label>Add ERC20 Contract Address</label>
+        <input />
+        <label>Select Chain</label>
+      </form>
+      <button onClick={console.log('Submit')}>Submit</button>
+      <section>Result</section>
+    </section>
+  );
+}
